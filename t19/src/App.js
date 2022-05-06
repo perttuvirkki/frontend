403ecd0500c14091e0f1b5167a353e50aa63a005
Perturbaattori <perttu.piiskaa@gmail.com>
2022-05-06 14:15:33 +0300

    t19 valmis

---
+++ t19/src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+
+function App() {
+  const GET_COUNTRIES = gql`
+    query GetCountries {
+      countries {
+        code
+        name
+        continent {
+          name
+        }
+      }
+    }
+  `;
+
+  const { loading, error, data } = useQuery(GET_COUNTRIES);
+
+  if (loading) return <p>Loading...</p>;
+  else if (error) return <p>Error...</p>;
+  else {
+    return (
+      <div className="App">
+        <table>
+          <tbody>
+            <th>CODE</th>
+            <th>NAME</th>
+            <th>CONTINENT</th>
+            {data.countries.map((country, index) => (
+              <tr key={index}>
+                <td>{country.code}</td>
+                <td>{country.name}</td>
+                <td>{country.continent.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
